c0694ccd6c5f7b069b02991316fc9b1d289dbbe3
DianRuth1208 <dianaruthsantos@gmail.com>
2019-11-13 20:51:37 -0600

    Preorden

--- Arboles.cpp
+++ Arboles.cpp
@@ -10,9 +10,10 @@ struct Nodo{
 };
 
 void menu();
-
 Nodo *crearNodo(int);
 void agregarNodo(Nodo *&,int);
+void mostrarArbol(Nodo *,int);
+void preOrden(Nodo*);
 
 Nodo *arbol = NULL;
 
@@ -25,25 +26,36 @@ int main(){
 
 
 void menu(){
-	int dato, opcion;
-cout<<"Agregar nodos a un arbol binario sin imprimir \n ";	
+	int dato, opcion,contador=0;
+cout<<"Se agrega al programa; Mostrar arbol y recorrido en Pre-Orden \n ";
 	do{
-		cout<<"\t ***M E N U***"<<endl;
+		cout<<"\t**M E N U***"<<endl;
 		cout<<"1.-Agregar un nodo"<<endl;
-		cout<<"2-Salir"<<endl;
+		cout<<"2.-Mostrar arbol"<<endl;
+		cout<<"3-Pre-Orden"<<endl;
+		cout<<"4-Salir"<<endl;
 		cout<<"Elige una opcion: ";
 		cin>>opcion;
 		
 		switch(opcion){
-			case 1: cout<<"\nIngresa un numero: ";
+			case 1: cout<<"\nDame un numero: ";
 					cin>>dato;
 					agregarNodo(arbol,dato);
 					cout<<"\nValor agregado correctamente \n ";
 					system("pause");
 					break;
+			case 2: cout<<"\nSe muestra el arbol:\n\n";
+					mostrarArbol(arbol,contador);
+					cout<<"\n";
+					system("pause");
+					break;
+			case 3: cout<<"\n Se muestra el recorrido en Pre-Orden: ";
+					preOrden(arbol);
+					cout<<"\n\n";
+					system("pause");
 		}
 		system("cls");
-	}while(opcion != 2);
+	}while(opcion != 4);
 }
 
 
@@ -72,4 +84,29 @@ void agregarNodo(Nodo *&arbol, int n){
 		}
 	}
 	
+}
+
+void mostrarArbol(Nodo *arbol, int cont){
+	if(arbol == NULL){
+		return;
+	}
+	else{
+		mostrarArbol(arbol->der,cont+1);
+		for(int i=0;i<cont;i++){
+			cout<<"   ";
+		}
+		cout<<arbol->dato<<endl;
+		mostrarArbol(arbol->izq,cont+1);
+	}
+}
+
+void preOrden(Nodo *arbol){
+	if(arbol == NULL){
+		return;
+	}
+	else{
+		cout<<arbol->dato<<" - ";
+		preOrden(arbol->izq);
+		preOrden(arbol->der);
+	}
 }
